0430c5585317b573c0a04e21748b698759e33fe4
student <gorshunov.oleg2017@yandex.ru>
2022-10-26 18:36:35 +0300

    Start

---
+++ Gorshunov/GorshunovLopushok/GorshunovLopushok/Domain/Entities/Product.cs
@@ -0,0 +1,94 @@
+﻿using GorshunovLopushok.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace GorshunovLopushok.Domain.Entities
+{
+    public partial class Product
+    {
+        private string? _image = null!;
+        private decimal _cost;
+        private string _materials = null!;
+        private StringBuilder _stringBuilder = new();
+
+        public Product()
+        {
+            ProductCostHistories = new HashSet<ProductCostHistory>();
+            ProductMaterials = new HashSet<ProductMaterial>();
+            ProductSales = new HashSet<ProductSale>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public int? ProductTypeId { get; set; }
+        public string ArticleNumber { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? Image
+        {
+            get => (_image == null) || (_image == string.Empty)
+                ? $"\\Resources\\picture.png"
+                : $"\\Resources{_image}";
+            set => _image = value;
+        }
+        public int? ProductionPersonCount { get; set; }
+        public int? ProductionWorkshopNumber { get; set; }
+        public decimal MinCostForAgent { get; set; }
+
+
+        [NotMapped]
+        public string? FullName
+        {
+            get
+            {
+                return $"{ProductType!.Title}"+ " | " +$"{Title}";
+            }
+        }
+
+        [NotMapped]
+        public decimal Cost
+        {
+            get
+            {
+                if (ProductMaterials.Count() == 0)
+                    return MinCostForAgent;
+
+                foreach (var pm in ProductMaterials)
+                {
+                    _cost += Math.Ceiling((decimal)pm.Count! * pm.Material.Cost);
+                }
+;
+                return _cost;
+            }
+        }
+
+        [NotMapped]
+        public string Materials
+        {
+            get 
+            {
+                _stringBuilder.Clear();
+                if (ProductMaterials.Count() == 0)
+                    return null!;
+
+                _stringBuilder.Append("Материалы: ");
+                foreach (var pm in ProductMaterials)
+                {
+                    _stringBuilder.Append(pm.Material.Title + ", ");
+                }
+                _stringBuilder.Remove(_stringBuilder.Length - 2, 2); ;
+                return _stringBuilder.ToString(); 
+            }
+        }
+
+
+
+        public virtual ProductType? ProductType { get; set; }
+        public virtual ICollection<ProductCostHistory> ProductCostHistories { get; set; }
+        public virtual ICollection<ProductMaterial> ProductMaterials { get; set; }
+        public virtual ICollection<ProductSale> ProductSales { get; set; }
+    }
+}
---
+++ Gorshunov/GorshunovLopushok/GorshunovLopushok/Presentation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GorshunovLopushok.Domain.Entities;
+using GorshunovLopushok.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GorshunovLopushok.Presentation.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public List<Product> Products { get; private set; }
+        public MainWindowViewModel()
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                Products = db.Products
+                .Include(pt => pt.ProductType)
+                .Include(p => p.ProductMaterials)
+                .ThenInclude(mp => mp.Material)
+                .ToList();
+            }
+        }
+    }
+}
